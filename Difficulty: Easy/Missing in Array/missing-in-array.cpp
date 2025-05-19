71582cba898a18becbb9401099a648d98d59ed64
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-05-20 00:50:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        int n = arr.size() + 1; // Because one number is missing from 1 to n
+        unordered_set<int> numSet(arr.begin(), arr.end());
+
+        for (int i = 1; i <= n; ++i) {
+            if (numSet.find(i) == numSet.end()) {
+                return i;
+            }
+        }
+
+        return -1; // Should not reach here if input is valid
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
